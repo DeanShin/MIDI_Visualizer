fd51f65010fb1f1f13863e3cd8dffe7f22fcda20
ShinkaiSetsuna <42852604+ShinkaiSetsuna@users.noreply.github.com>
2018-11-27 12:51:29 -0800

    Update MIDI_project.py

--- MIDI_project.py
+++ MIDI_project.py
@@ -1,7 +1,14 @@
 import mido
 
 pathToMidi = "./Fukashigi_no_Carte_Shinkai_Ver..mid"
+from note import Note
+
+notes = []
 
 mid = mido.MidiFile(pathToMidi)
-for msg in mid.play():
-    print(msg)
+    
+for msg in mid.play(meta_messages=True):
+    if ! msg.isMeta:
+        notes.append(Note(msg.channel, msg.type, msg.velocity, msg.time))
+    
+    
