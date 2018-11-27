0cc4d1ff3bf08b8d54cfa34abf0d76fd75e6afdb
ShinkaiSetsuna <42852604+ShinkaiSetsuna@users.noreply.github.com>
2018-11-27 12:27:22 -0800

    Update note.py

--- note.py
+++ note.py
@@ -3,18 +3,18 @@ class Note():
     def __init__(channel, note, velocity, time):
         self.height = 30
         self.width = 20
-
-        self.x = 20
-        self.y = 20
-
+        self.x = note * 20
+        self.y = 0
         self.change_x = 0
         self.change_y = 5
+        self.color = (0,0,128)
+        self.thickness = 1
     def move(self):
         self.x += self.change_x
         self.y += self.change_y
     def draw(self):
-        #draw note
-        pass
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), self.thickness)
+        
         
         
         
