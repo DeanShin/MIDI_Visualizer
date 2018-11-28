c65296caf0505273d1d955f1a5cfc2157d81e24f
ShinkaiSetsuna <42852604+ShinkaiSetsuna@users.noreply.github.com>
2018-11-27 16:15:37 -0800

    Update MIDI_project.py

--- MIDI_project.py
+++ MIDI_project.py
@@ -3,8 +3,9 @@ import mido
 pathToMidi = "./Fukashigi_no_Carte_Shinkai_Ver..mid"
 from note import Note
 
+screen_dims = (1760, 990)
+surface = pygame.display.set_mode(screen_dims)
 notes = []
-
 mid = mido.MidiFile(pathToMidi)
 
 # parse MIDI file and spawn notes in actual time
