490455284dc7440abd1f1f7533438385bb684e5a
ShinkaiSetsuna <42852604+ShinkaiSetsuna@users.noreply.github.com>
2018-11-29 10:35:36 -0800

    basic functions (stop growing and move)

--- note_object.py
+++ note_object.py
@@ -1,24 +1,27 @@
 class NoteObj():
     # this file holds the note class 
-    def __init__(self, type, time, channel, note, velocity):
-        self.height = 30
-        self.width = 20
-        self.x = note * 20
+    def __init__(self, x, channel, velocity):
+        self.height = 5
+        self.width = 20 
+        self.x = x * 20
         self.y = 0
         self.change_x = 0
         self.change_y = 5
         self.color = (0,0,128)
         self.thickness = 1
+        self.is_still_on = true
+        
+    def stop_growing(self):
+        self.is_still_on = false
+        
     def move(self):
         self.x += self.change_x
-        self.y += self.change_y
+        
+        if self.is_still_on:
+            self.height += self.change_y
+        else: 
+            self.y += self.change_y
+        
     def draw(self):
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), self.thickness)
-        
-        
-        
-        
-    
-
-        
-        
+       
